1e108d4273be9deeb586c889145e7f3678304026
Lindsay Mecher <lindsaymecher@gmail.com>
2020-03-12 10:44:29 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,46 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component{
+  constructor(){
+    super()
+    this.state={
+      timesClicked: 0
+    }
+  }
+
+  handleClick = (event) => {
+    // event.persist()
+    // console.log(event.target)
+    this.setState(previousState=> {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+  render(){
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.timesClicked}
+      </button>
+    )
+  }
+}
+
+export default DigitalClicker;
+
+
+
+
+// In the components/DigitalClicker.js file, create a DigitalClicker React component.
+//
+// This component has an initial state property called timesClicked, which is initially defined as 0.
+//
+// The component renders out a button with a label that shows the timesClicked value. This means that, at the start, your button should just say 0.
+//
+// Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+//
+// HINT: At the moment, src/index.js is trying to import DigitalClicker and the next component, YouTubeDebugger. To be able to see your progress as you build out DigitalClicker, you can comment out the second component by wrapping it in curly braces and comment indicators:
+//
+// {/*<YouTubeDebugger />*/}
+// Don't forget to remove these before you begin building the second component!
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component{
+  constructor(){
+    super()
+    this.state={
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrateButton = (event) => {
+    // console.log(event)
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  handleResolutionButton = (event) => {
+    // console.log(event.clientX, event.clientY)
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.video,
+          resolution: "720p"
+        }
+      }
+    })
+  }
+
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.handleBitrateButton} className="bitrate" >Bitrate</button>
+        <button onClick={this.handleResolutionButton} className="resolution">Resolution</button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger;
+
+
+// Create a button with the class 'bitrate'. Clicking this button changes the settings.bitrate state property to 12.
+//
+// Create a button with the class 'resolution'. Clicking this button changes the settings.video.resolution state property to '720p'.
